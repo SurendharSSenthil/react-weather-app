a8f0f875fd800e845b198c96606894e23574b435
SurendharSSenthil <surendharsenthil257@gmail.com>
2023-10-23 09:24:59 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const REACT_APP_API_URL = 'https://api.openweathermap.org/data/2.5/';
+const REACT_APP_API_KEY = 'a82a129e921c620cb24e5dbb68b7c603';
+
 function App() {
+  const [search, setSearch] = useState('Coimbatore');
+  const [temp, setTemp] = useState('');
+  const [desc,setDesc] = useState('');
+  let Weather = 0;
+  const handleSubmit = (e) => {
+    if (e.key === 'Enter') {
+      fetch(`${REACT_APP_API_URL}weather?q=${search}&appid=${REACT_APP_API_KEY}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          Weather = data.weather[0].main;
+          let x = data.main.temp;
+          setTemp(x.toFixed(1));
+          setDesc(data.weather[0].main);
+        })
+        .catch(err => {
+          console.log(err);
+          setTemp("City Not Found");
+        });
+    }
+  }
+
+  useEffect(() => {
+    handleSubmit({key : 'Enter'}); 
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof Weather != 'undefined') ? ((temp > 20) ? 'app-warm' : 'app-cold') : 'app-cold'}>
+      <div className='Body'>
+      <nav>
+        <input value={search} type='text' onChange={(e) => setSearch(e.target.value)} onKeyPress={handleSubmit} />
+      </nav>
+      <div className='info-container'> 
+        <div className='city-info'>{search}</div>
+        <div className='Temp'>{`${temp} C`}</div>
+      </div>
+      <div className='Desc'>{desc}</div> 
+      <div className='footer'>&copy;Surendhar Senthil</div>
+      </div>
     </div>
   );
 }
